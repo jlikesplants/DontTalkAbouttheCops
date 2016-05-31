440574ba615da3fa06e30257706913615847c35b
justine <justine.winnie@gmail.com>
2016-05-31 12:47:17 -0700

    successfully grabbing data from the counted api, tests passing

--- lib/the_counted_wrapper.rb
+++ lib/the_counted_wrapper.rb
@@ -1,34 +1,29 @@
-# GET http://thecountedapi.com/api/counted/?state=WA
-#
-# Params - all string
-# Name
-# Address
-# Age
-# Armed
-# Cause
-# City
-# State
-# Day
-# Month
-# Year
-# Race
-# Sex
+# [{"_id"=>"574ddf31e5b972652f2d7dc3",
+#   "name"=>"Michael Kocher Jr",
+#   "age"=>"19",
+#   "sex"=>"Male",
+#   "race"=>"White",
+#   "month"=>"January",
+#   "day"=>"3",
+#   "year"=>"2015",
+#   "address"=>"2600 Kaumualii Hwy",
+#   "city"=>"Kaumakani",
+#   "state"=>"HI",
+#   "cause"=>"Struck by vehicle",
+#   "dept"=>"Kauai Police Department",
+#   "armed"=>"No",
+#   "__v"=>0},
+
 
 
 # search for all deaths in 2015 in each state
 # return name, age, cause, state, race, sex
 
 module TheCountedWrapper
-  def self.name
+  API_ENDPOINT = "http://thecountedapi.com/api/counted"
 
+  def self.year(year)
+    response = HTTParty.get(API_ENDPOINT + "/?year=#{year}")
+    table = response.map { |row| { "name": row["name"], "age": row["age"], "sex": row["sex"], "race": row["race"], "year": row["year"], "state": row["state"], "cause": row["cause"] } }
   end
-
-  def self.date_range
-
-  end
-
-  def self.state
-
-  end
-
 end
--- test/wrappers/the_counted_wrapper_test.rb
+++ test/wrappers/the_counted_wrapper_test.rb
@@ -2,23 +2,23 @@ require 'test_helper'
 
 class TheCountedWrapperTest < ActiveSupport::TestCase
   setup do
-    @year = 2015
+    @year = "2015"
   end
 
-  it "should search for a year and return a table of fatality data" do
+  test "should search for a year and return a table of fatality data" do
     table = TheCountedWrapper.year(@year)
     assert_instance_of Array, table
     table.each do |row|
       assert_instance_of Hash, row
-      assert_includes row.keys, "year"
-      assert_instance_of String, row["year"]
+      assert_includes row.keys, :year
+      assert_instance_of String, row[:year]
     end
   end
 
-  it "should return data only from the year requested" do
+  test "should return data only from the year requested" do
     table = TheCountedWrapper.year(@year)
     table.each do |row|
-      assert_equal @year, row["year"]
+      assert_equal @year, row[:year]
     end
   end
 end
