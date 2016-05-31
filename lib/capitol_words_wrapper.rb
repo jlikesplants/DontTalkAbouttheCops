aa9a5cbec6c01ff390e9e372724c1658deb9386d
justine <justine.winnie@gmail.com>
2016-05-31 10:13:43 -0700

    capitol words wrapper and tests working

--- lib/capitol_words_wrapper.rb
+++ lib/capitol_words_wrapper.rb
@@ -18,13 +18,27 @@
 module CapitolWordsWrapper
 
   STATES = %w(WA CA VA)
+  API_ENDPOINT = "http://capitolwords.org/api/1"
 
   def self.phrase(phrase)
-    table = [
-      {state: "WA", count: 2},
-      {state: "CA", count: 4},
-      {state: "VA", count: 9}
-    ]
+    # table = [
+    #   {state: "WA", count: 2},
+    #   {state: "CA", count: 4},
+    #   {state: "VA", count: 9}
+    # ]
+
+    uri = Addressable::URI.parse(API_ENDPOINT + "/phrases/sta te.json")
+    uri.query_values = {
+      phrase: phrase,
+      sort: "count",
+      start_date: "2015-01-01",
+      end_date: "2015-12-31",
+      apikey: ENV["SUNLIGHT_FOUNDATION_KEY"]
+    }
+
+    table = HTTParty.get(uri.normalize.to_s).parsed_response["results"]
+    table.map { |row| row.merge("count" => row["count"].to_i) }.select { |row| STATES.include?(row["state"]) }
+
   end
 
   def self.state
--- test/wrappers/capitol_words_wrapper_test.rb
+++ test/wrappers/capitol_words_wrapper_test.rb
@@ -10,20 +10,20 @@ class CapitolWordsWrapperTest < ActiveSupport::TestCase
     assert_instance_of Array, table
     table.each do |row|
       assert_instance_of Hash, row
-      assert_includes row.keys, :state
-      assert_instance_of String, row[:state]
+      assert_includes row.keys, "state"
+      assert_instance_of String, row["state"]
       # assert row.keys.length == 2
-      assert_includes row.keys, :count
-      assert_instance_of Fixnum, row[:count]
+      assert_includes row.keys, "count"
+      assert_instance_of Fixnum, row["count"]
     end
   end
 
   test "returned table should include all states" do
     table = CapitolWordsWrapper.phrase(@phrase)
     states = table.map do |row|
-      row[:state]
+      row["state"]
     end.map(&:upcase).sort
-    assert_equal CapitolWordsWrapper::STATES.sort, states 
+    assert_equal CapitolWordsWrapper::STATES.sort, states
   end
 
 end
